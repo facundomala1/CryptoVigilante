8d47630648a836fd57fbb90f433b2669f83e90f1
facundomala1 <f.mala@alumno.um.edu.ar>
2025-11-26 10:35:09 -0300

    servidores,ok

---
+++ api.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI
+import csv
+
+app = FastAPI()
+
+ARCHIVO_CSV = "historial_bitcoin.csv"
+
+@app.get("/")
+def home():
+    return {"mensaje": "Bienvenido a la API de CryptoVigilante", "version": "1.0.0"}
+
+@app.get("/historial")
+def obtener_historial():
+    datos = []
+    try:
+        with open(ARCHIVO_CSV, mode='r') as file:
+            reader = csv.DictReader(file) 
+            for row in reader:
+                datos.append(row)
+        return datos
+    except FileNotFoundError:
+        return {"error": "Aún no hay datos guardados. Ejecuta monitor.py primero."}
+    
+# Endpoint 3: Obtener solo el último precio registrado (Lógica de negocio)
+@app.get("/ultimo")
+def obtener_ultimo_precio():
+    ultimo_dato = None
+    try:
+        with open(ARCHIVO_CSV, mode='r') as file:
+            reader = csv.DictReader(file)
+            # Recorremos todo para quedarnos con el final
+            for row in reader:
+                ultimo_dato = row
+        
+        if ultimo_dato:
+            return {
+                "moneda": "Bitcoin",
+                "precio_actual": f"${ultimo_dato['Precio (USD)']}",
+                "actualizado_a_las": ultimo_dato['Hora']
+            }
+        else:
+            return {"mensaje": "No hay datos aún"}
+            
+    except FileNotFoundError:
+        return {"error": "El sistema de monitoreo no está activo"}
